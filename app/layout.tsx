cb05bf6878845d201c48f3bfa1dbb44168341793
KrokoNinja <79910460+KrokoNinja@users.noreply.github.com>
2023-09-17 22:19:09 +0200

    implement background colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Lennert Pfundtner",
@@ -10,7 +13,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+				<div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+				<div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+				{children}
+			</body>
 		</html>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
 	return (
-		<div>
+		<main>
 			<h1>Hi, I'm Lennert Pfundtner</h1>
 			<p>
 				Frontend Developer from Germany studying Computer Science at IU Internationale Hochschule
 			</p>
-		</div>
+		</main>
 	);
 }
