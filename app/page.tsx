06619067fd324183aa72b8136882eff67ee484ed
KrokoNinja <79910460+KrokoNinja@users.noreply.github.com>
2023-09-18 20:33:56 +0200

    implement and style header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
 export default function Home() {
-	return (
-		<main>
-			<h1>Hi, I'm Lennert Pfundtner</h1>
-			<p>
-				Frontend Developer from Germany studying Computer Science at IU Internationale Hochschule
-			</p>
-		</main>
-	);
+	return <main></main>;
 }
